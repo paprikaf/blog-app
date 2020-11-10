90308906500e4b580b1cfc20af25d950d21cac1c
Ahmed Felfel <ahmedfelfel@Ahmeds-MacBook-Pro.local>
2020-11-10 12:08:59 -0500

    v1

---
+++ client/src/CommentList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default ( {comments} ) => {  
+    const rendererComments = comments.map(comment => {
+        let content;
+        if (comment.status === 'approved') {
+            content = comment.content;
+        }
+        if (comment.status === 'pending') {
+            content = 'this comment is awaiting moderation';
+        }
+        if (comment.status=== 'rejected') {
+            content= 'this comment has been rejected';
+        }
+        return <li key={comment.id}>{content}</li>
+    
+    })
+    
+    return <ul> {rendererComments} </ul> 
+    return <div></div>
+};
---
+++ event-bus/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require ('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+const events = [];
+
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    events.push(event);
+    
+    axios.post('http://posts-clusterip-srv:4000/events', event); //posts
+    axios.post('http://comments-clusterip-srv:4001/events', event); //comments
+    axios.post('http://query-clusterip-srv:4002/events', event); //query
+    axios.post('http://moderation-clusterip-srv:4003/events', event); // moderation
+    
+
+    res.send({status: 'OK'});
+})
+
+app.get('/events', (req, res) =>{
+    res.send(events);
+});
+
+app.listen(4005, () => {
+    console.log('listening on 4005');
+});
+ 
